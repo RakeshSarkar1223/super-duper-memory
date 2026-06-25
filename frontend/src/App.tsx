c3015ce48bc376930e405839f46877de135099ad
RakeshSarkar1223 <rakeshsarkar20066@gmail.com>
2026-06-25 09:36:17 +0530

    restructure project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 
 import './App.css'
-import Navbar from './Navbar'
+import Navbar from './landing_page/Navbar'
 
 function App() {
   // const [count, setCount] = useState(0)
