7510fa92688cee675abe0d0a56537f0b4449f9d4
RakeshSarkar1223 <rakeshsarkar20066@gmail.com>
2026-06-26 18:47:02 +0530

    Started working on dashboard

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-yellow-200 text-center text-3xl'>Hello from App</h1>
+    </>
+  )
+}
+
+export default App
